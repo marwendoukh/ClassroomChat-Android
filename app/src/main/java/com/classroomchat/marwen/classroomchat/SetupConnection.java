81650d859cbfcf202a150574169149698f37705e
Marwen Doukh <marwen.doukh@gmail.com>
2017-09-14 14:45:54 +0100

    First commit

---
+++ app/src/main/java/com/classroomchat/marwen/classroomchat/SetupConnection.java
@@ -0,0 +1,26 @@
+package com.classroomchat.marwen.classroomchat;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SetupConnection extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setup_connection, container, false);
+    }
+
+}
