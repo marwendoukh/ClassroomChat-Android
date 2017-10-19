ebab68074737e197c6dc741c81aec3550da04ecb
Marwen Doukh <marwen.doukh@gmail.com>
2017-10-19 12:08:35 +0100

    select user profile pic

--- app/src/main/java/com/classroomchat/marwen/classroomchat/SetUpProfile.java
+++ app/src/main/java/com/classroomchat/marwen/classroomchat/SetUpProfile.java
@@ -1,13 +1,63 @@
 package com.classroomchat.marwen.classroomchat;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class SetUpProfile extends AppCompatActivity {
+
+    ImageView userProfilePic;
+
+    ImageView userProfilePic;
+    private final int SELECT_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_up_profile);
+
+        userProfilePic = (ImageView) findViewById(R.id.preview_user_profile_picture_settingupaccount);
+
+        Button pickImage = (Button) findViewById(R.id.btn_pick);
+        pickImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+            }
+        });
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch (requestCode) {
+            case SELECT_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        final Uri imageUri = imageReturnedIntent.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                        userProfilePic.setImageBitmap(selectedImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+        }
+    }
+
 }
